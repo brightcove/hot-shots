edfb03c22d6e05b71a7e1919486ff062e08f36ea
Brian Deitte <brian.deitte@maxwellhealth.com>
2019-05-05 15:49:33 -0400

    Give more time for timer buffer for windows

--- test/timer.js
+++ test/timer.js
@@ -8,7 +8,7 @@ const testTypes = helpers.testTypes;
 const createServer = helpers.createServer;
 const createHotShotsClient = helpers.createHotShotsClient;
 
-const TIMER_BUFFER = 200;
+const TIMER_BUFFER = 300;
 
 describe('#timer', () => {
   let server;
